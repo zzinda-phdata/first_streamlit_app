f0c9204e10b962887416cdcab8dfdba6fc7bede4
zzinda-phdata <101811516+zzinda-phdata@users.noreply.github.com>
2023-02-17 10:37:38 -0600

    Update streamlit_app.py
    3.5

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
