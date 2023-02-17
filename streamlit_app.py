7c487c8828f78fe1c47768360099c2a29e645f6d
zzinda-phdata <101811516+zzinda-phdata@users.noreply.github.com>
2023-02-17 10:29:50 -0600

    Update streamlit_app.py
    3.3.2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 
 streamlit.title('My Parents Healthy Diner')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
