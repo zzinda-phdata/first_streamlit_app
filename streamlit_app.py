2337b7f585f6f8258b03acf0f99a8fa6a9808782
zzinda-phdata <101811516+zzinda-phdata@users.noreply.github.com>
2023-02-24 16:10:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-    streamlit.write('The user entered', fruit_choice)
+    #streamlit.write('The user entered', fruit_choice)
       
 except URLError as e:
   streamlit.error()
