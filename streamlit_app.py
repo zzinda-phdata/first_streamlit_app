a7d20d55fa89369833680ede616c8f5f318f8d93
zzinda-phdata <101811516+zzinda-phdata@users.noreply.github.com>
2023-02-24 16:37:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,17 +51,16 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
   
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    query = "insert into fruit_load_list values ('" + new_fruit + "')"
+    my_cur.execute(query)
+    return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(
+  streamlit.dataframe(my_data_rows)
   
-
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.dataframe(my_data_rows)
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-query = "INSERT INTO FRUIT_LOAD_LIST VALUES (" + add_my_fruit + ")"
-query = "insert into fruit_load_list values ('" + add_my_fruit + "')"
-my_cur.execute(query)
-streamlit.write('Thanks for adding', add_my_fruit)
+  
