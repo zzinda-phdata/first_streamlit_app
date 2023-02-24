4ee0da8e4987e80ed4230a3d132ed1c21e0e7d32
zzinda-phdata <101811516+zzinda-phdata@users.noreply.github.com>
2023-02-24 15:54:03 -0600

    Update streamlit_app.py
    12.7.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas 
 import requests
 import snowflake.connector
-import urllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents Healthy Diner')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
