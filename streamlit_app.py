06dd7e5d545f80cc40c4462d22352317bbe7dae0
zzinda-phdata <101811516+zzinda-phdata@users.noreply.github.com>
2023-02-24 15:10:18 -0600

    Update streamlit_app.py
    12.4

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,10 @@ streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.dataframe(pandas.json_normalize(fruityvice_response.json()))
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
